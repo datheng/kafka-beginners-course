777c9dd59826c3013294c20c8bab36207c253aa2
Datheng Wang <wmsisme@outlook.com>
2025-10-18 18:33:10 +0800

    feat:  Java Producer With Keys
    Simple java producer with keys.

---
+++ kafka-basics/src/main/java/io/github/datheng/demos/kafka/ProducerDemoKeys.java
@@ -0,0 +1,64 @@
+package io.github.datheng.demos.kafka;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoKeys {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoKeys.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Producer!");
+
+        // create Producer properties
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "127.0.0.1:9092");
+        props.put("key.serializer", StringSerializer.class.getName());
+        props.put("value.serializer", StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i <10; i++) {
+
+                String topic ="demo_java";
+                String key = "id_" +i;
+                String value = "hello world "+i;
+                // create a producer record
+                ProducerRecord<String, String> record =
+                        new ProducerRecord<String, String>(topic,key,value);
+
+                // send data
+                producer.send(record, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception exception) {
+                        // execute every time a record successfully sent or an exception is thrown
+                        if (exception == null) {
+                            log.info("Key: " + key + " Partition: " + metadata.partition());
+
+                        }else{
+                            log.error("Error while sending record", exception);
+                        }
+                    }
+                });
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        // tell the producer to send all data and block until done -- synchronous
+        producer.flush();
+        // flush and close producer
+        producer.close();
+    }
+}
